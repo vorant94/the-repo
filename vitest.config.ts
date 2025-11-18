e3e95e8c79fe4d1b5496444a9e04c7b1a3df3c27
Mordechai Dror <vorant94@pm.me>
2025-11-18 15:06:28 +0200

    upgrade more deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    workspace: ["{apps,libs,tools}/*/vitest.config.ts"],
+    projects: ["{apps,libs,tools}/*/vitest.config.ts"],
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
