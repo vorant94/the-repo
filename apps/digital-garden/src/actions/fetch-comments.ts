d29bd4feffa9ab8899c95f8ca09444d12a627e14
Mordechai Dror <vorant94@pm.me>
2025-12-08 22:46:48 +0200

    render comment author username alongside with comment text

--- apps/digital-garden/src/actions/fetch-comments.ts
+++ apps/digital-garden/src/actions/fetch-comments.ts
@@ -1,20 +1,10 @@
 import { defineAction } from "astro:actions";
-import { eq } from "drizzle-orm";
 import z from "zod";
-import { commentSchema, comments } from "../schema/comments";
+import { getComments } from "../lib/comments";
 
 export const fetchComments = defineAction({
   input: z.object({
     postSlug: z.string(),
   }),
-  handler: async (input, ctx) => {
-    const { db } = ctx.locals;
-
-    const rawComments = await db
-      .select()
-      .from(comments)
-      .where(eq(comments.postSlug, input.postSlug));
-
-    return rawComments.map((raw) => commentSchema.parse(raw));
-  },
+  handler: ({ postSlug }, ctx) => getComments(ctx, postSlug),
 });
