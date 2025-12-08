a7a9899f6b66f27e24f4ecb67ce603314a9c7afc
Mordechai Dror <vorant94@pm.me>
2025-12-09 00:06:29 +0200

    Add author info to comments and improve comment rendering

--- apps/digital-garden/src/actions/fetch-comments.ts
+++ apps/digital-garden/src/actions/fetch-comments.ts
@@ -1,10 +1,11 @@
 import { defineAction } from "astro:actions";
 import z from "zod";
-import { getComments } from "../lib/comments";
+import { type CommentWithAuthor, getComments } from "../lib/comments";
 
 export const fetchComments = defineAction({
   input: z.object({
     postSlug: z.string(),
   }),
-  handler: ({ postSlug }, ctx) => getComments(ctx, postSlug),
+  handler: ({ postSlug }, ctx): Promise<Array<CommentWithAuthor>> =>
+    getComments(ctx, postSlug),
 });
